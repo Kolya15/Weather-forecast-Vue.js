dc209241161d239bc29d92bcc7f9c0dea4bf9f7d
kolya15 <kolya.soroka15@gmail.com>
2020-11-24 14:37:17 +0200

    + moved project  + added axios

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { HTTP } from './api'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created () {
+    HTTP.get('weather?q=London&appid=7a44d3e3a278e49fbcb5dba603d8931e')
+  }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
